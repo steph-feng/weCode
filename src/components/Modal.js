057a4952fc138ce378c87a3a5588b8509583be4e
Stephanie Feng <stephanie.c.feng@gmail.com>
2024-04-06 20:37:14 -0700

    modal styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,26 +1,32 @@
+import "./Modal.css"
+
 export default function Modal({ setShowModal }) {
     const handleClick = () => {
         setShowModal(false);
     }
 
     return (
-        <div className="h-[70vh] w-[70vw] bg-white absolute flex flex-col items-center justify-center">
+        <div className="h-[70vh] w-[70vw] bg-white absolute">
 
-               <div className="w-[65vw] h-[4vh] bg-[#9DC49F] mt-10 rounded-r-2xl rounded-tl-xl"></div>
-                
-                <div className="flex flex-row h-full w-full">
-                    <div className="w-[4vh] h-[58vh] bg-[#74A376] rounded-b-2xl ml-9"></div>
+            <div className="w-[67vw] h-[4vh] bg-[#9DC49F] absolute ml-5 mt-10 rounded-2xl"></div>
+            <div className="w-[4vh] h-[65vh] bg-[#74A376] absolute ml-12 mt-5 rounded-2xl"></div>
 
-                    <div className="p-8 h-full w-full">
-                        <div>
-                            Upload five photos that represent you!
-                        </div>
-                        <input type="file" multiple id="upload" name="upload" accept="image/jpeg, image/jpg" />
-                        <button onClick={handleClick}>get your arc'venture starter pack</button>
+            <div className="absolute mt-28 ml-28 ">
+                <div className="flex flex-col items-center justify-center">
+                    <div className="title">
+                        you, but in five photos
                     </div>
-
+                    <input type="file" multiple id="upload" name="upload" accept="image/jpeg, image/jpg"
+                        className="mt-8"
+                    />
                 </div>
-       
+
+                <button className="bg-[#C2DEC4]" onClick={handleClick}>
+                    get your arc'venture starter pack
+                </button>
+            </div>
+
         </div>
     )
 }
+
