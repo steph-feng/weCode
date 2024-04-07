c195869121bb46059d09ae7fe7c4dadb1ca9254d
Stephanie Feng <stephanie.c.feng@gmail.com>
2024-04-06 17:14:49 -0700

    initial ui design

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,5 +1,29 @@
+import './LandingPage.css'
+
 export default function LandingPage() {
     return (
-        <div></div>
+        <div className="grid grid-cols-3 h-[100vh]">
+            <div className="slider col-span-2">
+
+                <div className="slide"></div>
+                <div className="slide"></div>
+                <div className="slide"></div>
+                <div className="slide"></div>
+
+                <div className='text'>
+                    <div className='title'>
+                        ARC'VENTURE
+                    </div>
+                    <div className='subtitle'>
+                        Explore the West Coast with Arc'teryx
+                    </div>
+                </div>
+
+            </div>
+            <div >
+                
+            </div>
+
+        </div>
     )
 }
